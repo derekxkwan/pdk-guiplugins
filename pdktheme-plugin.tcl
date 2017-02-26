7bd826f50e76f5d47cba3bae152af8ed71415830
Derek Kwan <derek.x.kwan@gmail.com>
2017-02-25 16:53:57 -0800

    lemon chiffon was too bright

--- pdktheme-plugin.tcl
+++ pdktheme-plugin.tcl
@@ -1,4 +1,4 @@
 #simple one-liner to change the patch window to a less eye-bleeding color
 #note that for tcl lines to be loaded, must be in your path and end with -plugin.tcl
 
-option add *PatchWindow*Canvas.background "lemon chiffon"
+option add *PatchWindow*Canvas.background "old lace"
