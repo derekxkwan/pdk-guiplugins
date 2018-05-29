1c1ca5c8f6b17b185e0cde27216c10a5e4243f46
Derek Kwan <derek.x.kwan@gmail.com>
2018-05-29 05:47:53 -0700

    color change

--- pdktheme-plugin.tcl
+++ pdktheme-plugin.tcl
@@ -1,4 +1,5 @@
 #simple one-liner to change the patch window to a less eye-bleeding color
 #note that for tcl lines to be loaded, must be in your path and end with -plugin.tcl
 
-option add *PatchWindow*Canvas.background "old lace"
+#option add *PatchWindow*Canvas.background "old lace"
+option add *PatchWindow*Canvas.background "mint cream"
