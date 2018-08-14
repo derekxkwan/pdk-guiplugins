c5899e3ef4c405120ead887fa41e08364e602f83
Derek Kwan <derek.x.kwan@gmail.com>
2018-08-14 06:57:01 -0700

    adding less tinge

--- pdktheme-plugin.tcl
+++ pdktheme-plugin.tcl
@@ -2,4 +2,5 @@
 #note that for tcl lines to be loaded, must be in your path and end with -plugin.tcl
 
 #option add *PatchWindow*Canvas.background "old lace"
-option add *PatchWindow*Canvas.background "mint cream"
+#option add *PatchWindow*Canvas.background "mint cream"
+option add *PatchWindow*Canvas.background "ghost white"
